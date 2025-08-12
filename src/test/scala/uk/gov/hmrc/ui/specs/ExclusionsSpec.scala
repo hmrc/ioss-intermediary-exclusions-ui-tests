a85a0432d015890c6695e62ca6feb31420013350
Lee Powell <lee.powell@digital.hmrc.gov.uk>
2025-08-12 10:47:21 +0100

    VEI-401 | Initial test repo

---
+++ src/test/scala/uk/gov/hmrc/ui/specs/ExclusionsSpec.scala
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.ui.specs
+
+import uk.gov.hmrc.ui.pages.Exclusions.Exclusions
+
+class ExclusionsSpec extends BaseSpec {
+
+  private val exclusions = Exclusions
+
+  Feature("Exclusions journeys") {
+
+    Scenario("Trader accesses the IOSS Intermediary Exclusions Service") {
+
+      Given("the trader accesses the Ioss Intermediary Exclusions Service")
+      exclusions.goToExclusionsJourney()
+    }
+  }
+
+}
